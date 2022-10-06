c83a8a27c034cf0fcd761bcdde8f60a8b6e6d3be
Adrian Guerrero <adrianson@outlook.com>
2022-10-06 11:33:51 -0600

    Error

---
+++ webApiAut/AplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using webApiAut.Entidades;
+
+namespace webApiAut
+{
+    public class AplicationDbContext : DbContext
+    {
+        public AplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Autor> Autores { get; set; }
+    }
+}
---
+++ webApiAut/Controllers/AutoresController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using webApiAut.Entidades;
+
+namespace webApiAut.Controllers
+{
+    [ApiController]
+    [Route("api/autores")]
+
+    public class AutoresController: ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Autor>> Get()
+        {
+            return new List<Autor>()
+            {
+                new Autor() { Id = 1, Nombre = "Adrian" },
+                new Autor() { Id = 2, Nombre = "Eduardo" }
+            };
+            }
+    }
+}
